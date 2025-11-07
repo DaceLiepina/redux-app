8ddd46c5c512e3ad07f1360075b197426dd106d9
Dace Liepina <dace.liepina77@gmail.com>
2025-11-07 19:43:59 +0100

    delete imports

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import type { JSX } from 'react'
 import './App.css'
 import Counter from './components/counter/Counter'
 import Sandwich from './components/Sandwich/Sandwich'
-import Tasks from './components/tasks/Tasks'
-import TaskCreation from './components/tasks/TaskCreation'
 import SmartTasks from './components/tasks/SmartTasks'
 
 function App():JSX.Element {
