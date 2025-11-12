cdf4d54efefbe67f00e078bfcddca988e4d1477c
Dace Liepina <dace.liepina77@gmail.com>
2025-11-12 19:42:09 +0100

    corrections

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,13 @@ function App():JSX.Element {
     
       <div>
         <img src="https://tse4.mm.bing.net/th/id/OIP.yHO9EA1tfmLQoatRkVA4_AHaFj?pid=Api&P=0&h=180" alt="redux img" className="mx-auto border-4 border-cream-200 rounded-lg"/>
+        <ProductCard/>
       <Counter/>
       <Sandwich/>
       <SmartTasks/>
       <DishForm/>
       <DishesList/>
-      <ProductCard/>
+      
       </div>
      
 
